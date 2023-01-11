7ea1ca396926844dec8a326b76be97901271471b
Rolly Maulana  Awangga <rolly@awang.ga>
2023-01-11 13:13:39 +0700

    ok

--- atmail.go
+++ atmail.go
@@ -1,99 +1,67 @@
 package atmail
 
 import (
-	"encoding/base64"
+	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
 )
 
-func GenerateToken() {
-	// Reads in our credentials
-	secret, err := ioutil.ReadFile("client_secret.json")
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
 	if err != nil {
-		log.Printf("Error: %v", err)
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
 	}
-
-	// Creates a oauth2.Config using the secret
-	// The second parameter is the scope, in this case we only want to send email
-	conf, err := google.ConfigFromJSON(secret, gmail.GmailSendScope)
-	if err != nil {
-		log.Printf("Error: %v", err)
-	}
-
-	// Creates a URL for the user to follow
-	url := conf.AuthCodeURL("CSRF", oauth2.AccessTypeOffline)
-	// Prints the URL to the terminal
-	fmt.Printf("Visit this URL: \n %v \n", url)
-
-	// Grabs the authorization code you paste into the terminal
-	var code string
-	_, err = fmt.Scan(&code)
-	if err != nil {
-		log.Printf("Error: %v", err)
-	}
-
-	// Exchange the auth code for an access token
-	tok, err := conf.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Printf("Error: %v", err)
-	}
-	fmt.Println(tok)
-	d1, _ := json.Marshal(tok)
-	err = os.WriteFile("tok.croot", d1, 0644)
+	return config.Client(context.Background(), tok)
 }
 
-func Sendemail() {
-	secret, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		log.Printf("Error: %v", err)
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
 	}
 
-	// Creates a oauth2.Config using the secret
-	// The second parameter is the scope, in this case we only want to send email
-	conf, err := google.ConfigFromJSON(secret, gmail.GmailSendScope)
+	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		log.Printf("Error: %v", err)
+		log.Fatalf("Unable to retrieve token from web: %v", err)
 	}
+	return tok
+}
 
-	tok, _ := os.ReadFile("tok.croot")
-	fmt.Println(tok)
-
-	// Create the *http.Client using the access token
-	var token oauth2.Token
-	json.Unmarshal(tok, &token)
-	client := conf.Client(oauth2.NoContext, &token)
-
-	// Create a new gmail service using the client
-	gmailService, err := gmail.New(client)
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
 	if err != nil {
-		log.Printf("Error: %v", err)
+		return nil, err
 	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
 
-	// New message for our gmail service to send
-	var message gmail.Message
-
-	// Compose the message
-	messageStr := []byte(
-		"From: youremail@gmail.com\r\n" +
-			"To: awangga@gmail.com\r\n" +
-			"Subject: My first Gmail API message\r\n\r\n" +
-			"Message body goes here!")
-
-	// Place messageStr into message.Raw in base64 encoded format
-	message.Raw = base64.URLEncoding.EncodeToString(messageStr)
-
-	// Send the message
-	_, err = gmailService.Users.Messages.Send("me", &message).Do()
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Printf("Error: %v", err)
-	} else {
-		fmt.Println("Message sent!")
+		log.Fatalf("Unable to cache oauth token: %v", err)
 	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
