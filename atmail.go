7aa1b36c6c02adece6c2fb0ca33530469d9bddb3
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-11 18:31:47 +0700

    v2

--- atmail.go
+++ atmail.go
@@ -2,6 +2,8 @@ package atmail
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,18 +11,68 @@ import (
 	"os"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
 )
 
+func GenerateGmailMessageWithAttachment(from string, to string, subject string, body string, fileMIMEType string, filename string, fileData string) (message gmail.Message) {
+	boundary := randStr(32, "alphanum")
+
+	messageBody := []byte("Content-Type: multipart/mixed; boundary=" + boundary + " \n" +
+		"MIME-Version: 1.0\n" +
+		"from: " + from + "\n" +
+		"to: " + to + "\n" +
+		"subject: " + subject + "\n\n" +
+
+		"--" + boundary + "\n" +
+		"Content-Type: text/html; charset=" + string('"') + "UTF-8" + string('"') + "\n" +
+		"MIME-Version: 1.0\n" +
+		"Content-Transfer-Encoding: 7bit\n\n" +
+		body + "\n\n" +
+		"--" + boundary + "\n" +
+
+		"Content-Type: " + fileMIMEType + "; name=" + string('"') + filename + string('"') + " \n" +
+		"MIME-Version: 1.0\n" +
+		"Content-Transfer-Encoding: base64\n" +
+		"Content-Disposition: attachment; filename=" + string('"') + filename + string('"') + " \n\n" +
+		chunkSplit(fileData, 76, "\n") +
+		"--" + boundary + "--")
+	message.Raw = base64.URLEncoding.EncodeToString(messageBody)
+	return message
+
+}
+
+func GetGmailService(secret string, accesstokenfile string, scope ...string) (srv *gmail.Service) {
+	ctx := context.Background()
+	b, err := os.ReadFile(secret)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, scope...)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := GetClient(config, accesstokenfile)
+
+	srv, err = gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+	return srv
+}
+
 // Retrieve a token, saves the token, then returns the generated client.
-func GetClient(config *oauth2.Config) *http.Client {
+func GetClient(config *oauth2.Config, accesstokenfile string) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
 	// created automatically when the authorization flow completes for the first
 	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
+	tok, err := tokenFromFile(accesstokenfile)
 	if err != nil {
 		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		saveToken(accesstokenfile, tok)
 	}
 	return config.Client(context.Background(), tok)
 }
@@ -64,4 +116,42 @@ func saveToken(path string, token *oauth2.Token) {
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
+}
+
+func randStr(strSize int, randType string) string {
+
+	var dictionary string
+
+	if randType == "alphanum" {
+		dictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	}
+
+	var strBytes = make([]byte, strSize)
+	_, _ = rand.Read(strBytes)
+	for k, v := range strBytes {
+		strBytes[k] = dictionary[v%byte(len(dictionary))]
+	}
+	return string(strBytes)
+}
+
+func chunkSplit(body string, limit int, end string) string {
+	var charSlice []rune
+
+	// push characters to slice
+	for _, char := range body {
+		charSlice = append(charSlice, char)
+	}
+
+	var result = ""
+
+	for len(charSlice) >= 1 {
+		if len(charSlice) < limit {
+			limit = len(charSlice)
+		} else {
+			result = result + string(charSlice[:limit]) + end
+			charSlice = charSlice[limit:]
+		}
+
+	}
+	return result
 }
