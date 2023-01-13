bd5a4445942b9a2d02e9eebe442cb40880bd2782
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-14 06:36:53 +0700

    type queue

--- type.go
+++ type.go
@@ -1,9 +1,9 @@
 package atmail
 
 type FileAttachment struct {
-	MIMEType string `json:"mimetype,omitempty" bson:"filemimetype,omitempty"`
-	Name     string `json:"name,omitempty" bson:"filename,omitempty"`
-	Base64   string `json:"base64,omitempty" bson:"base64string,omitempty"`
+	MIMEType string `json:"mimetype,omitempty" bson:"mimetype,omitempty"`
+	Name     string `json:"name,omitempty" bson:"name,omitempty"`
+	Base64   string `json:"base64,omitempty" bson:"base64,omitempty"`
 }
 
 type EmailMessage struct {
@@ -12,4 +12,15 @@ type EmailMessage struct {
 	Subject     string           `json:"subject,omitempty" bson:"subject,omitempty"`
 	Body        string           `json:"body,omitempty" bson:"body,omitempty"`
 	Attachments []FileAttachment `json:"attachments,omitempty" bson:"attachments,omitempty"`
+}
+
+type Success struct {
+	LabelIds []string `json:"labels,omitempty" bson:"labels,omitempty"`
+	Queue    Queue    `json:"queue,omitempty" bson:"queue,omitempty"`
+}
+
+type Queue struct {
+	Session string   `json:"session,omitempty" bson:"session,omitempty"`
+	Scope   []string `json:"scope,omitempty" bson:"scope,omitempty"`
+	Base64  string   `json:"base64,omitempty" bson:"base64,omitempty"`
 }
